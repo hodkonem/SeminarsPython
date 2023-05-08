eb69ca020afdd8b3ca16d116622619402420f77f
Mikhail Latypov <mikhaillat@yandex.ru>
2023-05-08 08:28:08 +0300

    Add task026

---
+++ task026.py
@@ -0,0 +1,18 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+# *Пример:*
+
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8 
+
+def recursive(base, exponent):
+    if exponent == 0:
+        return 1
+    else:
+        return base * recursive(base, exponent - 1)
+
+A = int(input("Введите число A: "))
+B = int(input("Введите число B: "))
+
+result = recursive(A, B)
+print(f"{A}^{B} = {result}")
